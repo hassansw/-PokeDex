ccac9b2958e7f83628330e87fb2f43316b73e7d6
Hassan Saleem <hassan_saleem@outlook.com>
2017-04-10 22:05:51 +0500

    Yeah

--- PokeDex/PokeCell.swift
+++ PokeDex/PokeCell.swift
@@ -10,4 +10,24 @@ import UIKit
 
 class PokeCell: UICollectionViewCell {
     
+    
+    
+    @IBOutlet weak var thumbImage : UIImageView!
+    @IBOutlet weak var nameLabel : UILabel!
+    
+    var pokemon : Pokemon!
+    
+    func configureCell( pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLabel.text = self.pokemon.name.capitalized
+        thumbImage.image = UIImage(named: "\(self.pokemon.pokedexId )")
+        
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        layer.cornerRadius = 5.0
+    }
 }

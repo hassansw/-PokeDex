fee8df9ab9f98f97d463f2eb36d4cc7444015321
Hassan Saleem <hassan_saleem@outlook.com>
2017-04-10 22:05:12 +0500

    Yeah

---
+++ PokeDex/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  PokeDex
+//
+//  Created by macbook on 03/04/2017.
+//  Copyright © 2017 TheWolves. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
---
+++ PokeDex/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  PokeDex
+//
+//  Created by macbook on 02/04/2017.
+//  Copyright © 2017 TheWolves. All rights reserved.
+//
+
+import Foundation
